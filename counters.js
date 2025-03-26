b22d59a78a501c1c4eef72a1cd614f07c4ad2db9
Queeneemm <99044971+Queeneemm@users.noreply.github.com>
2025-03-26 07:22:24 +0300

    2.0

---
+++ counters.js
@@ -0,0 +1,863 @@
+export const counters = {
+    "abaddon": [
+        "Brewmaster",
+        "Shadow Demon",
+        "Dark Seer",
+        "Elder Titan",
+        "Doom"
+    ],
+    "alchemist": [
+        "Chen",
+        "Brewmaster",
+        "Slark",
+        "Anti-Mage",
+        "Batrider"
+    ],
+    "ancient-apparition": [
+        "Brewmaster",
+        "Storm Spirit",
+        "Lycan",
+        "Phantom Lancer",
+        "Arc Warden"
+    ],
+    "anti-mage": [
+        "Huskar",
+        "Meepo",
+        "Axe",
+        "Lone Druid",
+        "Legion Commander"
+    ],
+    "arc-warden": [
+        "Meepo",
+        "Broodmother",
+        "Chen",
+        "Sand King",
+        "Io"
+    ],
+    "axe": [
+        "Timbersaw",
+        "Primal Beast",
+        "Batrider",
+        "Tiny",
+        "Venomancer"
+    ],
+    "bane": [
+        "Naga Siren",
+        "Chaos Knight",
+        "Phantom Lancer",
+        "Chen",
+        "Meepo"
+    ],
+    "batrider": [
+        "Tidehunter",
+        "Brewmaster",
+        "Sand King",
+        "Bane",
+        "Enigma"
+    ],
+    "beastmaster": [
+        "Medusa",
+        "Dazzle",
+        "Clinkz",
+        "Brewmaster",
+        "Bristleback"
+    ],
+    "bloodseeker": [
+        "Medusa",
+        "Storm Spirit",
+        "Nature's Prophet",
+        "Naga Siren",
+        "Juggernaut"
+    ],
+    "bounty-hunter": [
+        "Naga Siren",
+        "Meepo",
+        "Slark",
+        "Abaddon",
+        "Legion Commander"
+    ],
+    "brewmaster": [
+        "Lycan",
+        "Chen",
+        "Shadow Demon",
+        "Leshrac",
+        "Sand King"
+    ],
+    "bristleback": [
+        "Slark",
+        "Viper",
+        "Legion Commander",
+        "Juggernaut",
+        "Venomancer"
+    ],
+    "broodmother": [
+        "Earthshaker",
+        "Terrorblade",
+        "Meepo",
+        "Naga Siren",
+        "Crystal Maiden"
+    ],
+    "centaur-warrunner": [
+        "Lifestealer",
+        "Ember Spirit",
+        "Dazzle",
+        "Oracle",
+        "Witch Doctor"
+    ],
+    "chaos-knight": [
+        "Naga Siren",
+        "Sand King",
+        "Phantom Lancer",
+        "Tidehunter",
+        "Warlock"
+    ],
+    "chen": [
+        "Naga Siren",
+        "Slark",
+        "Phantom Lancer",
+        "Snapfire",
+        "Earth Spirit"
+    ],
+    "clinkz": [
+        "Phantom Lancer",
+        "Spectre",
+        "Naga Siren",
+        "Broodmother",
+        "Terrorblade"
+    ],
+    "clockwerk": [
+        "Huskar",
+        "Brewmaster",
+        "Meepo",
+        "Chen",
+        "Beastmaster"
+    ],
+    "crystal-maiden": [
+        "Rubick",
+        "Enigma",
+        "Juggernaut",
+        "Death Prophet",
+        "Dawnbreaker"
+    ],
+    "dark-seer": [
+        "Ursa",
+        "Sven",
+        "Oracle",
+        "Earthshaker",
+        "Ember Spirit"
+    ],
+    "dark-willow": [
+        "Naga Siren",
+        "Axe",
+        "Juggernaut",
+        "Arc Warden",
+        "Phantom Lancer"
+    ],
+    "dawnbreaker": [
+        "Silencer",
+        "Bristleback",
+        "Ancient Apparition",
+        "Dazzle",
+        "Elder Titan"
+    ],
+    "dazzle": [
+        "Slark",
+        "Phantom Lancer",
+        "Weaver",
+        "Morphling",
+        "Abaddon"
+    ],
+    "death-prophet": [
+        "Broodmother",
+        "Ancient Apparition",
+        "Undying",
+        "Lycan",
+        "Razor"
+    ],
+    "disruptor": [
+        "Chen",
+        "Shadow Demon",
+        "Nature's Prophet",
+        "Sniper",
+        "Oracle"
+    ],
+    "doom": [
+        "Batrider",
+        "Arc Warden",
+        "Meepo",
+        "Medusa",
+        "Spectre"
+    ],
+    "dragon-knight": [
+        "Viper",
+        "Venomancer",
+        "Ancient Apparition",
+        "Slark",
+        "Arc Warden"
+    ],
+    "drow-ranger": [
+        "Phantom Lancer",
+        "Mars",
+        "Centaur Warrunner",
+        "Earth Spirit",
+        "Chen"
+    ],
+    "earth-spirit": [
+        "Lone Druid",
+        "Meepo",
+        "Lifestealer",
+        "Naga Siren",
+        "Elder Titan"
+    ],
+    "earthshaker": [
+        "Templar Assassin",
+        "Viper",
+        "Huskar",
+        "Batrider",
+        "Hoodwink"
+    ],
+    "elder-titan": [
+        "Chen",
+        "Troll Warlord",
+        "Wraith King",
+        "Naga Siren",
+        "Enchantress"
+    ],
+    "ember-spirit": [
+        "Huskar",
+        "Meepo",
+        "Monkey King",
+        "Slark",
+        "Broodmother"
+    ],
+    "enchantress": [
+        "Marci",
+        "Phantom Assassin",
+        "Chaos Knight",
+        "Mars",
+        "Crystal Maiden"
+    ],
+    "enigma": [
+        "Rubick",
+        "Warlock",
+        "Medusa",
+        "Sand King",
+        "Kunkka"
+    ],
+    "faceless-void": [
+        "Meepo",
+        "Huskar",
+        "Naga Siren",
+        "Lone Druid",
+        "Slark"
+    ],
+    "grimstroke": [
+        "Spectre",
+        "Lycan",
+        "Visage",
+        "Sniper",
+        "Medusa"
+    ],
+    "gyrocopter": [
+        "Viper",
+        "Lifestealer",
+        "Underlord",
+        "Ancient Apparition",
+        "Bristleback"
+    ],
+    "hoodwink": [
+        "Timbersaw",
+        "Primal Beast",
+        "Spirit Breaker",
+        "Phantom Lancer",
+        "Windranger"
+    ],
+    "huskar": [
+        "Ancient Apparition",
+        "Viper",
+        "Necrophos",
+        "Chaos Knight",
+        "Naga Siren"
+    ],
+    "invoker": [
+        "Puck",
+        "Lycan",
+        "Broodmother",
+        "Templar Assassin",
+        "Batrider"
+    ],
+    "io": [
+        "Troll Warlord",
+        "Ancient Apparition",
+        "Batrider",
+        "Monkey King",
+        "Broodmother"
+    ],
+    "jakiro": [
+        "Rubick",
+        "Pangolier",
+        "Faceless Void",
+        "Keeper of the Light",
+        "Ringmaster"
+    ],
+    "juggernaut": [
+        "Riki",
+        "Templar Assassin",
+        "Visage",
+        "Morphling",
+        "Anti-Mage"
+    ],
+    "keeper-of-the-light": [
+        "Huskar",
+        "Storm Spirit",
+        "Anti-Mage",
+        "Lycan",
+        "Slardar"
+    ],
+    "kunkka": [
+        "Lifestealer",
+        "Ember Spirit",
+        "Undying",
+        "Faceless Void",
+        "Venomancer"
+    ],
+    "legion-commander": [
+        "Winter Wyvern",
+        "Shadow Demon",
+        "Dawnbreaker",
+        "Omniknight",
+        "Tusk"
+    ],
+    "leshrac": [
+        "Undying",
+        "Medusa",
+        "Queen of Pain",
+        "Drow Ranger",
+        "Outworld Destroyer"
+    ],
+    "lich": [
+        "Pugna",
+        "Undying",
+        "Brewmaster",
+        "Shadow Demon",
+        "Weaver"
+    ],
+    "lifestealer": [
+        "Razor",
+        "Morphling",
+        "Nature's Prophet",
+        "Broodmother",
+        "Slardar"
+    ],
+    "lina": [
+        "Axe",
+        "Templar Assassin",
+        "Bristleback",
+        "Batrider",
+        "Mars"
+    ],
+    "lion": [
+        "Templar Assassin",
+        "Batrider",
+        "Beastmaster",
+        "Omniknight",
+        "Lina"
+    ],
+    "lone-druid": [
+        "Naga Siren",
+        "Brewmaster",
+        "Underlord",
+        "Monkey King",
+        "Earthshaker"
+    ],
+    "luna": [
+        "Bristleback",
+        "Dark Seer",
+        "Underlord",
+        "Medusa",
+        "Chen"
+    ],
+    "lycan": [
+        "Naga Siren",
+        "Chen",
+        "Phantom Lancer",
+        "Broodmother",
+        "Sven"
+    ],
+    "magnus": [
+        "Enigma",
+        "Ember Spirit",
+        "Kez",
+        "Puck",
+        "Huskar"
+    ],
+    "marci": [
+        "Troll Warlord",
+        "Magnus",
+        "Pangolier",
+        "Winter Wyvern",
+        "Dark Willow"
+    ],
+    "mars": [
+        "Chen",
+        "Lifestealer",
+        "Faceless Void",
+        "Anti-Mage",
+        "Puck"
+    ],
+    "medusa": [
+        "Anti-Mage",
+        "Dark Seer",
+        "Slark",
+        "Alchemist",
+        "Centaur Warrunner"
+    ],
+    "meepo": [
+        "Elder Titan",
+        "Winter Wyvern",
+        "Earthshaker",
+        "Underlord",
+        "Troll Warlord"
+    ],
+    "mirana": [
+        "Terrorblade",
+        "Meepo",
+        "Naga Siren",
+        "Bloodseeker",
+        "Shadow Demon"
+    ],
+    "monkey-king": [
+        "Batrider",
+        "Techies",
+        "Phoenix",
+        "Ancient Apparition",
+        "Muerta"
+    ],
+    "morphling": [
+        "Nyx Assassin",
+        "Doom",
+        "Axe",
+        "Timbersaw",
+        "Meepo"
+    ],
+    "naga-siren": [
+        "Leshrac",
+        "Axe",
+        "Tidehunter",
+        "Necrophos",
+        "Sand King"
+    ],
+    "natures-prophet": [
+        "Spectre",
+        "Broodmother",
+        "Phantom Assassin",
+        "Templar Assassin",
+        "Morphling"
+    ],
+    "necrophos": [
+        "Ancient Apparition",
+        "Leshrac",
+        "Doom",
+        "Oracle",
+        "Shadow Demon"
+    ],
+    "night-stalker": [
+        "Naga Siren",
+        "Broodmother",
+        "Beastmaster",
+        "Huskar",
+        "Lone Druid"
+    ],
+    "nyx-assassin": [
+        "Slardar",
+        "Chen",
+        "Lifestealer",
+        "Sven",
+        "Visage"
+    ],
+    "ogre-magi": [
+        "Phantom Lancer",
+        "Naga Siren",
+        "Chaos Knight",
+        "Anti-Mage",
+        "Enigma"
+    ],
+    "omniknight": [
+        "Shadow Demon",
+        "Ancient Apparition",
+        "Phantom Lancer",
+        "Doom",
+        "Razor"
+    ],
+    "oracle": [
+        "Lycan",
+        "Razor",
+        "Marci",
+        "Undying",
+        "Anti-Mage"
+    ],
+    "outworld-destroyer": [
+        "Arc Warden",
+        "Nyx Assassin",
+        "Chaos Knight",
+        "Visage",
+        "Phantom Lancer"
+    ],
+    "pangolier": [
+        "Huskar",
+        "Monkey King",
+        "Bloodseeker",
+        "Primal Beast",
+        "Elder Titan"
+    ],
+    "phantom-assassin": [
+        "Timbersaw",
+        "Axe",
+        "Primal Beast",
+        "Pudge",
+        "Earth Spirit"
+    ],
+    "phantom-lancer": [
+        "Sand King",
+        "Dark Seer",
+        "Warlock",
+        "Leshrac",
+        "Necrophos"
+    ],
+    "phoenix": [
+        "Huskar",
+        "Juggernaut",
+        "Lina",
+        "Lifestealer",
+        "Muerta"
+    ],
+    "primal-beast": [
+        "Grimstroke",
+        "Underlord",
+        "Enigma",
+        "Clockwerk",
+        "Lifestealer"
+    ],
+    "puck": [
+        "Night Stalker",
+        "Riki",
+        "Broodmother",
+        "Huskar",
+        "Tiny"
+    ],
+    "pudge": [
+        "Marci",
+        "Pangolier",
+        "Timbersaw",
+        "Lycan",
+        "Kez"
+    ],
+    "pugna": [
+        "Chen",
+        "Chaos Knight",
+        "Pudge",
+        "Huskar",
+        "Anti-Mage"
+    ],
+    "queen-of-pain": [
+        "Ember Spirit",
+        "Medusa",
+        "Huskar",
+        "Puck",
+        "Anti-Mage"
+    ],
+    "razor": [
+        "Morphling",
+        "Queen of Pain",
+        "Anti-Mage",
+        "Primal Beast",
+        "Storm Spirit"
+    ],
+    "riki": [
+        "Naga Siren",
+        "Underlord",
+        "Bristleback",
+        "Tiny",
+        "Bloodseeker"
+    ],
+    "rubick": [
+        "Templar Assassin",
+        "Tusk",
+        "Earth Spirit",
+        "Enchantress",
+        "Hoodwink"
+    ],
+    "sand-king": [
+        "Zeus",
+        "Lifestealer",
+        "Centaur Warrunner",
+        "Storm Spirit",
+        "Phoenix"
+    ],
+    "shadow-demon": [
+        "Chen",
+        "Meepo",
+        "Timbersaw",
+        "Lone Druid",
+        "Naga Siren"
+    ],
+    "shadow-fiend": [
+        "Zeus",
+        "Lina",
+        "Spirit Breaker",
+        "Wraith King",
+        "Primal Beast"
+    ],
+    "shadow-shaman": [
+        "Phantom Lancer",
+        "Terrorblade",
+        "Omniknight",
+        "Meepo",
+        "Arc Warden"
+    ],
+    "silencer": [
+        "Lone Druid",
+        "Lycan",
+        "Phantom Lancer",
+        "Broodmother",
+        "Abaddon"
+    ],
+    "skywrath-mage": [
+        "Lycan",
+        "Medusa",
+        "Chen",
+        "Templar Assassin",
+        "Marci"
+    ],
+    "slardar": [
+        "Naga Siren",
+        "Terrorblade",
+        "Phantom Lancer",
+        "Drow Ranger",
+        "Troll Warlord"
+    ],
+    "slark": [
+        "Disruptor",
+        "Leshrac",
+        "Elder Titan",
+        "Mars",
+        "Grimstroke"
+    ],
+    "snapfire": [
+        "Lifestealer",
+        "Faceless Void",
+        "Huskar",
+        "Sven",
+        "Templar Assassin"
+    ],
+    "sniper": [
+        "Storm Spirit",
+        "Spectre",
+        "Primal Beast",
+        "Pudge",
+        "Centaur Warrunner"
+    ],
+    "spectre": [
+        "Abaddon",
+        "Meepo",
+        "Beastmaster",
+        "Tidehunter",
+        "Undying"
+    ],
+    "spirit-breaker": [
+        "Underlord",
+        "Clockwerk",
+        "Naga Siren",
+        "Tiny",
+        "Necrophos"
+    ],
+    "storm-spirit": [
+        "Meepo",
+        "Anti-Mage",
+        "Pugna",
+        "Bane",
+        "Oracle"
+    ],
+    "sven": [
+        "Troll Warlord",
+        "Phoenix",
+        "Templar Assassin",
+        "Bristleback",
+        "Lifestealer"
+    ],
+    "techies": [
+        "Lifestealer",
+        "Templar Assassin",
+        "Medusa",
+        "Juggernaut",
+        "Spectre"
+    ],
+    "templar-assassin": [
+        "Phantom Lancer",
+        "Batrider",
+        "Troll Warlord",
+        "Naga Siren",
+        "Medusa"
+    ],
+    "terrorblade": [
+        "Axe",
+        "Phantom Lancer",
+        "Winter Wyvern",
+        "Dark Seer",
+        "Warlock"
+    ],
+    "tidehunter": [
+        "Razor",
+        "Death Prophet",
+        "Juggernaut",
+        "Elder Titan",
+        "Huskar"
+    ],
+    "timbersaw": [
+        "Pugna",
+        "Silencer",
+        "Outworld Destroyer",
+        "Medusa",
+        "Faceless Void"
+    ],
+    "tinker": [
+        "Anti-Mage",
+        "Pugna",
+        "Nyx Assassin",
+        "Juggernaut",
+        "Leshrac"
+    ],
+    "tiny": [
+        "Necrophos",
+        "Lifestealer",
+        "Viper",
+        "Underlord",
+        "Huskar"
+    ],
+    "treant-protector": [
+        "Timbersaw",
+        "Chen",
+        "Phoenix",
+        "Juggernaut",
+        "Batrider"
+    ],
+    "troll-warlord": [
+        "Axe",
+        "Slark",
+        "Winter Wyvern",
+        "Razor",
+        "Ringmaster"
+    ],
+    "tusk": [
+        "Naga Siren",
+        "Sand King",
+        "Elder Titan",
+        "Timbersaw",
+        "Luna"
+    ],
+    "underlord": [
+        "Rubick",
+        "Lifestealer",
+        "Juggernaut",
+        "Ursa",
+        "Huskar"
+    ],
+    "undying": [
+        "Medusa",
+        "Luna",
+        "Clinkz",
+        "Meepo",
+        "Naga Siren"
+    ],
+    "ursa": [
+        "Phantom Lancer",
+        "Naga Siren",
+        "Wraith King",
+        "Troll Warlord",
+        "Riki"
+    ],
+    "vengeful-spirit": [
+        "Phantom Lancer",
+        "Terrorblade",
+        "Chaos Knight",
+        "Dark Willow",
+        "Tidehunter"
+    ],
+    "venomancer": [
+        "Storm Spirit",
+        "Tinker",
+        "Phantom Lancer",
+        "Arc Warden",
+        "Dark Seer"
+    ],
+    "viper": [
+        "Phantom Lancer",
+        "Naga Siren",
+        "Terrorblade",
+        "Arc Warden",
+        "Dark Seer"
+    ],
+    "visage": [
+        "Chen",
+        "Winter Wyvern",
+        "Bristleback",
+        "Naga Siren",
+        "Axe"
+    ],
+    "void-spirit": [
+        "Meepo",
+        "Io",
+        "Huskar",
+        "Tidehunter",
+        "Dragon Knight"
+    ],
+    "warlock": [
+        "Tidehunter",
+        "Weaver",
+        "Lycan",
+        "Bristleback",
+        "Sand King"
+    ],
+    "weaver": [
+        "Phantom Lancer",
+        "Faceless Void",
+        "Naga Siren",
+        "Morphling",
+        "Rubick"
+    ],
+    "windranger": [
+        "Axe",
+        "Centaur Warrunner",
+        "Mars",
+        "Bane",
+        "Clockwerk"
+    ],
+    "winter-wyvern": [
+        "Timbersaw",
+        "Pugna",
+        "Sand King",
+        "Anti-Mage",
+        "Ancient Apparition"
+    ],
+    "witch-doctor": [
+        "Medusa",
+        "Riki",
+        "Bristleback",
+        "Muerta",
+        "Templar Assassin"
+    ],
+    "wraith-king": [
+        "Phantom Lancer",
+        "Necrophos",
+        "Underlord",
+        "Tidehunter",
+        "Dark Seer"
+    ],
+    "zeus": [
+        "Chen",
+        "Meepo",
+        "Batrider",
+        "Void Spirit",
+        "Storm Spirit"
+    ]
+};
